2d7c458620d311fd57169b1a6294aa5411c70f25
Daniel Shmidov <46876824+danielshmidov@users.noreply.github.com>
2022-01-03 02:24:02 -0800

    feat: colors and JS

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-//global selections are variables 
-const colorDivs = document.querySelectorAll('.color');
-const generateButton = document.querySelectorAll('.generate');
+//global selections are variables
+const colorDivs = document.querySelectorAll(".color");
+const generateButton = document.querySelectorAll(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
-const currentHexes = document.querySelectorAll('.color h2');
-
-
-
+const currentHexes = document.querySelectorAll(".color h2");
 
 //functions
 
-
-
 //color generator
 function generateHex() {
-    const letters = ["#123456789ABCDEF"]
-    let hash = '#'
-    for (let i = 0; i <6; i++) {
-        hash = hash + letters[math.floor(Math.random() * 16)];
-    }
-    return hash;
+  const letters = "#123456789ABCDEF";
+  let hash = "#";
+  for (let i = 0; i < 6; i++) {
+    hash = hash + letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
 }
 
 function randomColors() {
-    
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    console.log(hexText);
+    const randomColor = generateHex();
+
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
 }
+
+randomColors();
