aa608d6ec39dc6091272de89db465582c06c871b
Daniel Shmidov <46876824+danielshmidov@users.noreply.github.com>
2022-01-09 00:45:14 -0800

    feat: icons and text now change color based on user input

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const generateButton = document.querySelectorAll(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 
+//event listeners
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControl);
+});
+colorDivs.forEach((div, index) => {
+  div.addEventListener("change", () => {
+    updateTextUI(index);
+  });
+});
+
 //functions
 
 //color generator
@@ -47,9 +57,52 @@ function colorizeSlider(color, hue, saturation, brightness) {
   const noSat = color.set("hsl.s", 0);
   const fullSat = color.set("hsl.s", 1);
   const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //scale brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
 
   //update input colors
-  saturation.style.backgroundImage = `linear-Gradient(to right,${scaleSat(0)}, ${scaleSat(1)})`;
+  saturation.style.backgroundImage = `linear-Gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-Gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)}, ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,75,204),rgb(204,75,204))`;
+}
+
+function hslControl(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+
+  let slider = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = slider[0];
+  const brightness = slider[1];
+  const saturation = slider[2];
+
+  const bgColor = colorDivs[index].querySelector("h2").innerText;
+
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.h", hue.value)
+    .set("hsl.l", brightness.value);
+
+  colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUI(index) {
+  const activeDiv = colorDivs[index];
+  const color = chroma(activeDiv.style.backgroundColor);
+  const textHex = activeDiv.querySelector("h2");
+  const icons = activeDiv.querySelectorAll('.controls button');
+  textHex.innerText = color.hex();
+  checkTextContrast(color, textHex);
+  for(icon of icons) {
+    checkTextContrast(color, icon);
+  }
+  
 }
 
 randomColors();
